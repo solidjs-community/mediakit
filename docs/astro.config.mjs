db222370e443ce745a9906ad4348ba718a98908f
OrJDev <orjdeveloper@gmail.com>
2023-08-07 14:15:07 +0300

    netlify adapter

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/static";
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), mdx()],
   site: `https://astro.build`,
-  output: "static",
-  adapter: vercel()
-});
+  output: 'static',
+  adapter: netlify(),
+})

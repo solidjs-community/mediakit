ac9646b14688eaba8beb6c59c9361183fcb458fc
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-08-14 23:41:03 +0100

    Add initial (definitely not complete) open-graph package

---
+++ packages/open-graph/rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'es',
+    },
+  ],
+  plugins: [
+    typescript()
+  ]
+}

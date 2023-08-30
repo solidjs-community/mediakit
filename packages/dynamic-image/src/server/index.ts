aa121ebbac56d445f6077074241a1179dfe265c3
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-08-30 20:09:26 +0100

    Use `satori-html` instead of manually parsing the HTML and converting

--- packages/dynamic-image/src/server/index.ts
+++ packages/dynamic-image/src/server/index.ts
@@ -1,52 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ImageResponse } from '@vercel/og'
-import HTMLParser from 'html-to-json-parser'
-import type { JSONContent } from 'html-to-json-parser/dist/types'
+import { html } from 'satori-html'
 import type { JSX } from 'solid-js'
-function convertStyles(styleString?: string) {
-  if (!styleString) return
-  const obj: any = {}
-  styleString.split(';').forEach((e) => {
-    const [key, value] = e.split(':')
-    obj[key] = value
-  })
-  return obj
-}
-function convertHTML(json: JSONContent | string): any {
-  if (typeof json === 'string') {
-    return json
-  }
-  return {
-    type: json.type,
-    props: {
-      children:
-        json.content.length === 1
-          ? convertHTML(json.content[0])
-          : json.content.map((c) => convertHTML(c)),
-      ...json.attributes,
-      style:
-        json.attributes && 'style' in json.attributes
-          ? convertStyles(json.attributes?.style as string)
-          : undefined,
-    },
-  }
-}
+
 export async function createOpenGraphImage(
   jsx?:
     | string
     | JSX.Element
     | (() => (string | JSX.Element) | Promise<string | JSX.Element>)
+    | { t: string }
 ) {
-  // TODO: Fix this hack
-  let jsx1 = jsx as any
-  if (typeof jsx1 === 'function') {
-    jsx1 = await jsx1()
+  if (!jsx) return
+  if (typeof jsx === 'function') {
+    jsx = await jsx()
   }
-  if (typeof jsx1 !== 'string') {
-    jsx1 = jsx1.t as string
+  if (jsx && typeof jsx === 'object' && Object.hasOwn(jsx, 't')) {
+    jsx = (jsx as any).t as string
   }
-  const parsed = await HTMLParser(jsx1)
-  const converted = convertHTML(parsed as JSONContent)
-  return new ImageResponse(converted)
+  if (typeof jsx !== 'string') {
+    return
+  }
+  const converted = html(jsx)
+  return new ImageResponse(converted as any)
 }
 
 export async function createBase64Image(jsx?: string | JSX.Element) {

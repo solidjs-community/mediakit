e584a22aff79037c567bf82759edd329c7f99142
OrJDev <orjdeveloper@gmail.com>
2024-04-03 21:38:09 +0300

    fix: export callMiddleware$

--- packages/prpc/src/utils.ts
+++ packages/prpc/src/utils.ts
@@ -1,5 +1,4 @@
 import { ZodSchema } from 'zod'
-import { FilterOutResponse, IMiddleware, InferFinalMiddlware } from './types'
 
 export const validateZod = async <Schema extends ZodSchema>(
   payload: any,
@@ -18,7 +17,6 @@ export const error$ = (error: any, init?: ResponseInit): Response => {
   const headers = new Headers(init?.headers)
   headers.set('Content-Type', 'application/json')
   headers.set('X-Prpc-Error', '1')
-  headers.set('X-Prpc-Validation', '1')
   return new Response(
     JSON.stringify({
       error: typeof error === 'string' ? { message: error } : error,
@@ -28,54 +26,4 @@ export const error$ = (error: any, init?: ResponseInit): Response => {
       headers,
     }
   ) as any
-}
-
-export const middleware$ = <
-  Mw extends IMiddleware<CurrentContext>,
-  CurrentContext = unknown
->(
-  mw: Mw
-): Mw => {
-  return mw
-}
-
-type Flattened<T> = T extends Array<infer U> ? Flattened<U> : T
-
-export const pipe$ = <
-  CurrentMw extends IMiddleware<any> | IMiddleware<any>[],
-  Mw extends IMiddleware<FilterOutResponse<InferFinalMiddlware<CurrentMw>>>[]
->(
-  currentMw: CurrentMw,
-  ...middlewares: Mw
-): Flattened<Mw> => {
-  if (Array.isArray(currentMw)) {
-    return [...currentMw, ...middlewares].flat() as any
-  }
-  return [currentMw, ...middlewares].flat() as any
-}
-
-export const hideEvent = <T>(ctx$: T, fully?: boolean) => {
-  if (typeof ctx$ === 'object' && ctx$ !== null && 'event$' in ctx$) {
-    if (fully) {
-      delete (ctx$ as any).event$
-    } else {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { event$: _$ignore, ...rest } = ctx$ as any
-      return rest
-    }
-  }
-  return ctx$
-}
-
-export const hideRequest = <T>(ctx$: T, fully?: boolean) => {
-  if (typeof ctx$ === 'object' && ctx$ !== null && 'event$' in ctx$) {
-    if (fully) {
-      delete (ctx$ as any).event$
-    } else {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { event$: _$ignore, ...rest } = ctx$ as any
-      return rest
-    }
-  }
-  return ctx$
 }

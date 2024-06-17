5e74c388f360f8c5a894213fc2b437c52c1095d7
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-06-17 23:17:14 +0100

    Add back OG

---
+++ packages/og/tests/fixtures/multiple-children/output.js
@@ -0,0 +1,22 @@
+import { createOpenGraphImage } from '@solid-mediakit/og/server'
+import { createMemo } from 'solid-js'
+const DynamicImage1 = (props) => {
+  const img = (...args) => {
+    'use server'
+
+    return createOpenGraphImage(
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    )
+  }
+  const url = createMemo(() => {
+    return (
+      img.url.replace('_server', '_server/') +
+      `&raw=true&args=${encodeURIComponent(JSON.stringify(props.values))}`
+    )
+  })
+  return <>{url()}</>
+}
+const coolVar = <DynamicImage1 values={[]} />

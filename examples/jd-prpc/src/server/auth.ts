90da872cfb2a94dcb626add0727b3047c2c1cc0e
OrJDev <orjdeveloper@gmail.com>
2025-03-21 18:16:30 +0200

    more auth fixes

--- examples/jd-prpc/src/server/auth.ts
+++ examples/jd-prpc/src/server/auth.ts
@@ -1,14 +1,13 @@
-import { type SolidAuthConfig } from "@solid-mediakit/auth";
-import Discord from "@auth/core/providers/discord";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./db";
-import { serverEnv } from "~/env/server";
+import { type SolidAuthConfig } from '@solid-mediakit/auth'
+import Discord from '@auth/core/providers/discord'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from './db'
 
-declare module "@auth/core/types" {
+declare module '@auth/core/types' {
   export interface Session {
     user: {
-      id: string;
-    } & DefaultSession["user"];
+      id: string
+    } & DefaultSession['user']
   }
 }
 
@@ -16,18 +15,18 @@ export const authOptions: SolidAuthConfig = {
   callbacks: {
     session({ session, user }) {
       if (session.user) {
-        session.user.id = user.id;
+        session.user.id = user.id
       }
-      return session;
+      return session
     },
   },
   adapter: PrismaAdapter(prisma),
   providers: [
     Discord({
-      clientId: serverEnv.DISCORD_ID,
-      clientSecret: serverEnv.DISCORD_SECRET,
+      clientId: process.env.DISCORD_ID,
+      clientSecret: process.env.DISCORD_SECRET,
     }),
   ],
   debug: false,
   basePath: import.meta.env.VITE_AUTH_PATH,
-};
+}

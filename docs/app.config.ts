f697835f8980b84e353abed720199964f975a896
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-05-29 12:36:58 +0100

    fix(docs): prerender all pages

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -4,7 +4,25 @@ import { createWithSolidBase } from "@kobalte/solidbase/config";
 import defaultTheme, {
 } from "@kobalte/solidbase/default-theme";
 import { defineConfig } from "@solidjs/start/config";
-import { SidebarItem } from "@kobalte/solidbase/client";
+import { readdir } from "node:fs/promises";
+const getPaths = async (base: string): Promise<string[]> => {
+	const files = await readdir(base, { withFileTypes: true });
+	const paths = [];
+	for (const file of files) {
+		if (file.isDirectory()) {
+			paths.push(...((await getPaths(path.join(base, file.name)))));
+		}
+		else {
+			paths.push(path.join(base, file.name))
+		}
+	}
+	return paths;
+}
+const getRoutes = async (base: string) => {
+	const paths = await getPaths(base);
+	const routes = paths.map(p => p.replace(path.normalize(base), "").replaceAll("\\", "/").replace(".mdx", ""));
+	return routes;
+}
 
 export default defineConfig(
 	createWithSolidBase(defaultTheme)(
@@ -17,6 +35,7 @@ export default defineConfig(
 
 				},
 				prerender: {
+					routes: await getRoutes("src/routes"),
 					crawlLinks: true,
 				},
 			},
@@ -26,8 +45,7 @@ export default defineConfig(
 			lang: "en",
 			themeConfig: {
 				socialLinks: {
-					// @ts-ignore
-					github: "https://github.com/solidjs-community/mediakit",
+					github: { link: "https://github.com/solidjs-community/mediakit" },
 				},
 				nav: [
 					{

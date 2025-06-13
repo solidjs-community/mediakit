d2e0cda0a7fb1b8d7feb5625357bd09dbee1b9e0
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-06-13 17:00:01 +0100

    docs: remove unnecessary font loading code

--- docs/src/routes/packages/og/DynamicImageExample.tsx
+++ docs/src/routes/packages/og/DynamicImageExample.tsx
@@ -1,19 +1,11 @@
 import { DynamicImage, Image } from "@solid-mediakit/og";
 import { createSignal } from "solid-js";
-import notoSans from "../../../../node_modules/@vercel/og/dist/noto-sans-v27-latin-regular.ttf?arraybuffer"
 
-const fonts = [
-	{
-		name: "Noto Sans",
-		weight: 500,
-		data: notoSans,
-	}
-]
 export function Component() {
 	const [count, setCount] = createSignal(0);
 	return <div>
 		<Image>
-			<DynamicImage imageOptions={{ fonts }}>
+			<DynamicImage>
 				<div
 					style={{
 						width: '100%',
@@ -30,12 +22,12 @@ export function Component() {
 			</DynamicImage>
 		</Image>
 		<button
-        onClick={() => {
-          setCount(c => c += 1) // The component above automatically detects this change,
-          // and the image will rerender on the client
-        }}
-      >
-        Increment
-      </button>
+			onClick={() => {
+				setCount(c => c += 1) // The component above automatically detects this change,
+				// and the image will rerender on the client
+			}}
+		>
+			Increment
+		</button>
 	</div>
 }
